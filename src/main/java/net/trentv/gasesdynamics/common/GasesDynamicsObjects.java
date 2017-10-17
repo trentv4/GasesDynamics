65ffcf022e0927f4263fad54f36f92ae5165d1f8
trentv4 <trentvanslyke@gmail.com>
2017-10-17 17:02:35 -0400

    Convert gas thrower to proper Action and NBT, like it's supposed to be from the start.

--- src/main/java/net/trentv/gasesdynamics/common/GasesDynamicsObjects.java
+++ src/main/java/net/trentv/gasesdynamics/common/GasesDynamicsObjects.java
@@ -6,7 +6,7 @@ import net.trentv.gasesdynamics.common.item.ItemGasthrower;
 public class GasesDynamicsObjects
 {
 	public static final ItemGasthrower GAS_THROWER = new ItemGasthrower();
-	
+
 	public static void init()
 	{
 		GasesDynamicsRegistry.registerItem(GAS_THROWER);
--- src/main/java/net/trentv/gasesdynamics/common/item/ItemGasthrower.java
+++ src/main/java/net/trentv/gasesdynamics/common/item/ItemGasthrower.java
@@ -1,25 +1,27 @@
 package net.trentv.gasesdynamics.common.item;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.trentv.gasesdynamics.GasesDynamics;
 import net.trentv.gasesframework.api.GFManipulationAPI;
+import net.trentv.gasesframework.api.GFRegistrationAPI;
 import net.trentv.gasesframework.api.GasType;
 
 public class ItemGasthrower extends Item
 {
-	private GasType heldGas;
-	private int fuel = 0;
-	private int distance = 1;
-	
 	public ItemGasthrower()
 	{
 		setRegistryName(GasesDynamics.MODID, "gasthrower");
@@ -27,39 +29,123 @@ public class ItemGasthrower extends Item
 		setUnlocalizedName("gasthrower");
 	}
 
+	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
 	{
-		if(!world.isRemote)
-		{
-			if(!player.isSneaking())
-			{
-				for(int i = 0; i < distance; i++)
-				{
-					Vec3d pos = player.getPositionEyes(0f).add(player.getLookVec().scale(i + 2));
-					GFManipulationAPI.addGasLevel(new BlockPos(pos), world, heldGas, 8);
+		player.setActiveHand(hand);
+		return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+	}
 
-					if(distance < 5) distance++;
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+	{
+		if (!worldIn.isRemote)
+		{
+			if (player.isSneaking())
+			{
+				if (GFManipulationAPI.getGasLevel(pos, worldIn) > 0)
+				{
+					ItemStack stack = player.getHeldItem(hand);
+					NBTTagCompound nbt = getNBT(stack);
+					if (!nbt.hasKey("gastype"))
+						nbt.setString("gastype", "gasesframework:air");
+					nbt.setString("gastype", GFManipulationAPI.getGasType(pos, worldIn).getRegistryName().toString());
 				}
 			}
 		}
-		
-		return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+		return EnumActionResult.PASS;
 	}
-	
-    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-    {
-    	if(!worldIn.isRemote)
-    	{
-        	if(player.isSneaking())
-        	{
-            	if(GFManipulationAPI.getGasLevel(pos, worldIn) > 0)
-            	{
-            		heldGas = GFManipulationAPI.getGasType(pos, worldIn);
-            		fuel += 10;
-            		System.out.println("Gassing up: " + heldGas.name + " at " + fuel);
-            	}
-        	}
-    	}
-        return EnumActionResult.PASS;
-    }
+
+	@Override
+	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase player, int timeLeft)
+	{
+		if (!worldIn.isRemote)
+		{
+			NBTTagCompound nbt = getNBT(player.getActiveItemStack());
+			nbt.setInteger("distance", 1);
+		}
+	}
+
+	@Override
+	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
+	{
+		if (entity instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer) entity;
+			if (player.isHandActive())
+			{
+				if (!world.isRemote)
+				{
+					NBTTagCompound nbt = getNBT(stack);
+					if (!nbt.hasKey("fuel"))
+						nbt.setInteger("fuel", 0);
+					if (!nbt.hasKey("distance"))
+						nbt.setInteger("distance", 1);
+					if (!nbt.hasKey("gastype"))
+						nbt.setString("gastype", "gasesframework:air");
+
+					int fuel = nbt.getInteger("fuel");
+					int distance = nbt.getInteger("distance");
+					GasType heldGas = GFRegistrationAPI.getGasType(new ResourceLocation(nbt.getString("gastype")));
+					if (heldGas == null)
+						return;
+
+					if (!player.isSneaking())
+					{
+						for (int i = 0; i < distance; i++)
+						{
+							Vec3d pos = player.getPositionEyes(0f).add(player.getLookVec().scale(i + 3));
+							BlockPos bpos = new BlockPos(pos);
+							if (!GFManipulationAPI.canPlaceGas(bpos, world, heldGas))
+								break;
+
+							if (fuel >= 16)
+							{
+								GFManipulationAPI.addGasLevel(bpos, world, heldGas, 16);
+								fuel -= 16;
+							}
+							else if (fuel > 0)
+							{
+								GFManipulationAPI.addGasLevel(bpos, world, heldGas, fuel);
+								fuel = 0;
+							}
+
+						}
+						if (distance < 12)
+							distance++;
+					}
+
+					nbt.setInteger("fuel", fuel);
+					nbt.setInteger("distance", distance);
+					nbt.setString("gastype", heldGas.getRegistryName().toString());
+				}
+			}
+		}
+	}
+
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack)
+	{
+		return Integer.MAX_VALUE;
+	}
+
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack)
+	{
+		return EnumAction.NONE;
+	}
+
+	private NBTTagCompound getNBT(ItemStack stack)
+	{
+		if (stack.hasTagCompound())
+		{
+			return stack.getTagCompound();
+		}
+		else
+		{
+			NBTTagCompound nbt = new NBTTagCompound();
+			stack.setTagCompound(nbt);
+			return nbt;
+		}
+	}
 }
