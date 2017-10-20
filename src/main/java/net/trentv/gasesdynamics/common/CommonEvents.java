c5e64f306be276954eac6e7f7242754ce1676440
trentv4 <trentvanslyke@gmail.com>
2017-10-20 12:45:16 -0400

    Move entity registration to common, not client, events.

--- src/main/java/net/trentv/gasesdynamics/common/CommonEvents.java
+++ src/main/java/net/trentv/gasesdynamics/common/CommonEvents.java
@@ -1,6 +1,16 @@
 package net.trentv.gasesdynamics.common;
 
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.trentv.gasesdynamics.GasesDynamics;
+import net.trentv.gasesdynamics.common.entity.projectile.EntityGasGrenade;
+
 public class CommonEvents
 {
-
+	@SubscribeEvent
+	public void registerEntities(RegistryEvent.Register<EntityEntry> event)
+	{
+		event.getRegistry().register(new EntityEntry(EntityGasGrenade.class, "gas_grenade").setRegistryName(GasesDynamics.MODID, "gas_grenade"));
+	}
 }
